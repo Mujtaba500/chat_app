7183f9fe8420e70aad26a1e63a2323fdb48c51cc
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2024-08-21 13:01:05 +0500

    Add logout functionality

--- client/src/components/sidebar/LogoutButton.tsx
+++ client/src/components/sidebar/LogoutButton.tsx
@@ -1,14 +1,26 @@
 import { LogOut } from "lucide-react";
+import toast from "react-hot-toast";
+import axiosInstance from "../../axios/Axios";
+import { useAuthContext } from "../../context/AuthContext";
 
 const LogoutButton = () => {
-	const logout = () => {
-		alert("You are logged out");
-	};
+  const { setAuthUser } = useAuthContext();
+  const logout = async () => {
+    try {
+      const response = await axiosInstance.post("/auth/logout");
+      toast.success(response.data.message);
+      setAuthUser(null);
+    } catch (err: any) {
+      console.log("status: ", err.response.status);
+      console.log("Error: ", err.response.data.message);
+      toast.error(err.response.data.message);
+    }
+  };
 
-	return (
-		<div className='mt-auto'>
-			<LogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
-		</div>
-	);
+  return (
+    <div className="mt-auto">
+      <LogOut className="w-6 h-6 text-white cursor-pointer" onClick={logout} />
+    </div>
+  );
 };
 export default LogoutButton;
